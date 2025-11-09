38d6e7364a68b66af5063aae59ca02bbfeb3ebb2
IsaacST08 <isaacshiellsthomas@me.com>
2025-11-09 02:13:20 -0700

    Updated pins to match the new wiring diagram

--- src/pins.h
+++ src/pins.h
@@ -1,17 +1,17 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define RED_LED_PIN 2
-#define GREEN_LED_PIN 3
-#define YELLOW_LED_PIN 4
-#define BLUE_LED_PIN 5
+#define RED_LED_PIN 0
+#define GREEN_LED_PIN 3  // Removed
+#define YELLOW_LED_PIN 1
+#define BLUE_LED_PIN 2
 
 // Stepper Motor Pins
-#define SM_ENABLE_PIN 20  // Stepper Motor Enable Pin
-#define SM_DIR_PIN 19     // Stepper Motor Direction Pin
+#define SM_ENABLE_PIN 17  // Stepper Motor Enable Pin
+#define SM_DIR_PIN 20     // Stepper Motor Direction Pin
 #define SM_PULSE_PIN 18   // Stepper Motor Pulse Pin
-#define SM_MS1_PIN 10     // Stepper Motor Micro-Step Pin A
-#define SM_MS2_PIN 11     // Stepper Motor Micro-Step Pin B
+#define SM_MS1_PIN 16     // Stepper Motor Micro-Step Pin A
+#define SM_MS2_PIN 19     // Stepper Motor Micro-Step Pin B
 
 // Limit Switch Pins
 #define LS_1 12  // Limit switch 1
