774dfa0f04d01fb73de4f048130df4ba7e41d662
IsaacST08 <isaacshiellsthomas@me.com>
2025-12-12 16:48:26 -0700

    Created helper math functions

--- code/src/common.h
+++ code/src/common.h
@@ -4,4 +4,18 @@
 #define RIGHT_SIDE 0
 #define LEFT_SIDE 1
 
+typedef bool direction_t;
+
+#ifndef MAX
+#define MAX(a, b) ((a > b) ? (a) : (b))
+#endif
+
+#ifndef MIN
+#define MIN(a, b) ((a < b) ? (a) : (b))
+#endif
+
+#ifndef CLAMP
+#define CLAMP(val, upper, lower) (MAX(MIN(val, upper), lower))
+#endif
+
 #endif
